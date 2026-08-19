953f73098314f9f6c54ca56c22db5d1499f02892
4vp69hrn4m-del <4vp69hrn4m@privaterelay.appleid.com>
2026-08-19 23:14:01 +0900

    route.ts を更新

--- app/api/svs-rounds/[id]/route.ts
+++ app/api/svs-rounds/[id]/route.ts
@@ -21,6 +21,7 @@ export async function GET(
       participants: {
         include: { timeSlots: { include: { timeSlot: true } } },
       },
+      rankings: { orderBy: { rank: "asc" } },
     },
   });
   if (!round) {
